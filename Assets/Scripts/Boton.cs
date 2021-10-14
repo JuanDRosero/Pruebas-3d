05dc789e8e29bc41727d4af269e0969719bcd27c
Juan David <jdrosero59@gmail.com>
2021-10-14 08:32:57 -0500

    Ya interaciona

---
+++ Assets/Scripts/Boton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour, IAction
+{
+    [SerializeField]
+    private Color activateColor;
+    [SerializeField]
+    private Color inactivateColor;
+    [SerializeField]
+    private Material material;
+
+    private bool activated;
+    public void Activate()
+    {
+        activated = !activated;
+        if (activated)
+        {
+            material.color = activateColor;
+        }
+        else
+        {
+            material.color = inactivateColor;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material.color = inactivateColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
